80295ef1dbc4e670677a2ade2e3e965d3b44bdc8
taylordehart <60669527+taylordehart@users.noreply.github.com>
2020-05-31 06:29:23 +0300

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,11 @@
+var superheroes = require("superheroes");
+var supervillain = require("supervillains");
+
+
+
+var mySuperheroName = superheroes.random();
+var mySupervillianName = supervillain.random();
+
+
+console.log(mySuperheroName);
+console.log(mySupervillianName);
